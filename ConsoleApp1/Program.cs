b74697f61e591591ec764fb2d53989eec6f2795c
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-11-27 13:46:22 +0700

    Tasks 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// дана строка be, or not to be?
+    /// найди дубликаты слов
+    /// 
+    /// вывод be
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string freeProposal = "be, or not to be?";
+
+            var words = freeProposal.Split(' ', '.', '!', '?', ',');
+
+            //Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            
+            ////foreach (var word in words)
+            ////{
+            ////    if (!String.IsNullOrWhiteSpace(word))
+            ////    {
+            ////        if (!wordCounts.ContainsKey(word))
+            ////        {
+            ////            wordCounts[word]++;
+            ////        }
+            ////        else
+            ////        {
+            ////            wordCounts.Add(word, 1);
+            ////        }
+            ////    }
+            ////}
+
+            ////var repatedWords = wordCounts.Where(kvp.Value > 1).Select(kvp => kvp.Key);
+
+
+
+
+        }
+    }
+}
